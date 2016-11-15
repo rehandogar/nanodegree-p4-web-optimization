451d1171dd5bf83e4f29eac8899b09d9e6afee5a
REHAN-PC\Arbisoft <rehanumardogar@gmail.com>
2016-11-15 19:41:04 +0500

    new optimizations: 0-  All html files are now free of render blocking css and scripts. 1- Scrolling in cam's pizza page is now running on 60fps. 2- Little optimization in selector of resize pizza method. [ Need to refractor it to achieved the 5ms threshold]

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -406,13 +406,13 @@ var resizePizzas = function(size) {
   function changeSliderLabel(size) {
     switch(size) {
       case "1":
-        document.querySelector("#pizzaSize").innerHTML = "Small";
+        document.getElementById("pizzaSize").innerHTML = "Small";
         return;
       case "2":
-        document.querySelector("#pizzaSize").innerHTML = "Medium";
+        document.getElementById("pizzaSize").innerHTML = "Medium";
         return;
       case "3":
-        document.querySelector("#pizzaSize").innerHTML = "Large";
+        document.getElementById("pizzaSize").innerHTML = "Large";
         return;
       default:
         console.log("bug in changeSliderLabel");
@@ -501,10 +501,12 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+  var items = document.getElementsByClassName('mover');
+  var scrollPos = document.body.scrollTop;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    var itemLeft = items[i].basicLeft;
+    var phase = Math.sin((scrollPos / 1250) + (i % 5));
+    items[i].style.left = itemLeft + 100 * phase + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
